ae28dded14d96ebc4aa509815524bd484b787011
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-03-13 15:54:18 -0700

    main page modified to include tech stacks and footer

--- src/App.tsx
+++ src/App.tsx
@@ -29,14 +29,31 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React Deployed on Github Pages with Github Actions</h1>
+      <h1>Successful Deployment!</h1>
+      <section className='stack-list'>
+        <h3>Website Stack: </h3>
+        <p>React</p>
+        <p>Vite</p>
+        <p>TypeScript</p>
+        <p>CICD Pipeline</p>
+        <p>GitHub Pages</p>
+        <p>GitHub Actions</p>
+      </section>
+      <section className='medium-article'>
+        <h3>Learn how to deploy this site: </h3>
+        <a src=''>Medium Article by Chihiro & Justin</a>
+      </section>
+
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <footer>
+        <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+        <p>Template deployed by Chihiro & Justin Snider @2023</p>
+      </footer>
     </div>
   );
 }
